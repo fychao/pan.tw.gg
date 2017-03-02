f300897a7c346044b3a8a259020e6af3b8238511
drink891121 <pan.tw.gg@gmail.com>
2017-03-02 22:32:55 +0800

    更新hw3
    check ansewer and check input not finish yet

--- HomeWork/HW3.c
+++ HomeWork/HW3.c
@@ -5,44 +5,93 @@
 
 //Function: Check the number match the condition or not.
 int CheckNum(int num);
+
 //Function: Check the answer that user input and return nAnB.
 //int CheckAns(int answer, int input);
+
 //Function: Bubble Sort.
 void BubbleSort(int *alist, int max_idx);
-//Functoin: Binary Search.!!!!!not done!!!
-//int BinarySearch(int);
 
 int main(void)
 {
-	// setting random seed
-	int idx;
-	 
+	int idx=0;
+	//set random seed 
 	time_t t;
 	srand((unsigned) time(&t));
 	
 	int *int_list;
 	int_list=(int *)malloc(sizeof(int));
-	
-	for(idx=0;idx<10;idx++)
+        
+        // make a number	
+        int num = *(int_list+idx*sizeof(int));
+	for(idx=0;idx<1;idx++)
 	{
 
 		//while (int returned_CheckNum)
-		
-		*(int_list+idx*sizeof(int))=rand()%100;
-		printf("int_list=%d\n",*(int_list+idx*sizeof(int))) ;
-		int returned_CheckNum= CheckNum(*(int_list+idx*sizeof(int)));
-		printf("%d\n",returned_CheckNum);
+		int returned_CheckNum = 0;
+                while(returned_CheckNum == 0)
+                {
+		    *(int_list+idx*sizeof(int))=rand()%10000;
+		    printf("int_list=%d\n",*(int_list+idx*sizeof(int))) ;
+		    returned_CheckNum= CheckNum(*(int_list+idx*sizeof(int)));
+		    printf("%d\n",returned_CheckNum);
+                    num = *(int_list+idx*sizeof(int));
+                }                
+                printf("Game Started!!\n");                                  
+                printf("1.the answer=%d\n", *(int_list+idx*sizeof(int)));
+                
 	}
+
+        //change the number into array
+        printf("2.the answer=%d\n", num);
+        int a,b,c,d,e,f,g,h;
+	a=num;
+	b=num%1000;
+	c=num%100;
+	d=num%10;
 	
+	e=d;//ones
+	f=(c-d)/10;//tens 
+	g=(b-c)/100;//hundred
+	h=(a-b)/1000;//thousand
+        
+        int p[3];
+	p[0]=h;
+        p[1]=g;
+        p[2]=f;
+        p[3]=e;
+        for(idx=0;idx<4;idx++)
+        {
+            printf("p[%d]=%d\n",idx,p[idx]);
+        }
+
+            
+
+        //user input
+        int user_input[4];
+        printf("Guess A Number!!!~~\n");
+        for(idx=0;idx<4;idx++)
+        {
+            scanf(" %d",&user_input[idx]);
+        }
+
+        for(idx=0;idx<4;idx++)
+        {
+            printf("user_input[%d]=%d\n",idx,user_input[idx]);
+        }
+
+
+        /*
+	//buble sort
 	BubbleSort(int_list, 10);
 	
 	printf("After Sorting:\n");
 	
 	for(idx=0;idx<10;idx++)
 	{
-		printf("idx=%d, int_list=%d\n",idx,*(int_list+idx*sizeof(int)));
+            printf("idx=%d, int_list=%d\n",idx,*(int_list+idx*sizeof(int)));
 	}
-	
+	*/
 	
 	
 	
@@ -60,12 +109,12 @@ int CheckNum(int num)
 	c=num%100;
 	d=num%10;
 	
-	e=d;//個位 
-	f=(c-d)/10;//十位 
-	g=(b-c)/100;//百位 
-	h=(a-b)/1000;//千位 
+	e=d;//ones
+	f=(c-d)/10;//tens 
+	g=(b-c)/100;//hundred
+	h=(a-b)/1000;//thousand
 	
-	if(e!=f && e!=g && e!=h && f!=g && f!=h && g!=h) 
+	if(e!=f && e!=g && e!=h && f!=g && f!=h && g!=h && a>=1234) 
 	    return 1;
 	else
 	    return 0;
@@ -94,12 +143,12 @@ void BubbleSort(int *alist, int max_idx)
 
 
 
-/*
-int CheckAns(int answer, int input)
+int CheckAns(int answer[4], int input[4])
 {
-}
+    int i,j;
+    for(i=0;i<4;i++)
+    {
 
-int BinarySearch(int)//not done yet
-{
+    }
+    
 }
-*/
